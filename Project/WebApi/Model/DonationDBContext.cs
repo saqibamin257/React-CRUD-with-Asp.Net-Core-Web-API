c9fd61b8df5839dc4d3a5ed0d2d8ca1fc8dae775
Saqib Amin <saqibamin257@gmail.com>
2021-01-22 14:48:12 +0000

    Initial commit

---
+++ Project/WebApi/Model/DonationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
